a9ea621f0565a1bb706c101cb9a94ffa98bb63ca
Emma <emmagray183@gmail.com>
2019-11-08 17:37:29 +0000

    passes insufficient funds test and all homework completed

--- pet_shop.rb
+++ pet_shop.rb
@@ -80,9 +80,12 @@ end
 def sell_pet_to_customer(shop, pet, customer)
   if pet == nil
     return 0
-  end 
-  add_pet_to_customer(customer, pet)
-  increase_pets_sold(shop, 1)
-  remove_customer_cash(customer, pet[:price])
-  add_or_remove_cash(shop, pet[:price])
+  elsif customer_can_afford_pet(customer, pet)
+    add_pet_to_customer(customer, pet)
+    increase_pets_sold(shop, 1)
+    remove_customer_cash(customer, pet[:price])
+    add_or_remove_cash(shop, pet[:price])
+  else
+    return 0
+  end
 end
